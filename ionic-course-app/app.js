1af0a11ccb822431c4c3cf9ac4b74ce32cf0a695
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-04-02 17:11:07 -0300

    added alert

--- ionic-course-app/app.js
+++ ionic-course-app/app.js
@@ -3,6 +3,8 @@ const courseRatingInput = document.querySelector('#input-course-rating');
 const addButton = document.querySelector('#btn-add');
 const courseList = document.querySelector('#course-list');
 
+const alertCtrl = document.querySelector('ion-alert');
+
 addButton.addEventListener('click', () => {
     const enteredCourseName = courseNameInput.value;
     const enteredCourseRating = courseRatingInput.value;
@@ -11,6 +13,17 @@ addButton.addEventListener('click', () => {
         enteredCourseRating.trim().length <= 0 ||
         enteredCourseRating < 1 ||
         enteredCourseRating > 5) {
+        alertCtrl.header = 'Invalid inputs';
+        alertCtrl.subHeader = 'Error';
+        alertCtrl.message = 'Please enter a correct course name and rating.';
+        alertCtrl.buttons = ['OK'];
+
+        document.body.appendChild(alertCtrl);
+        alertCtrl.present();
+
+        const { role } = alertCtrl.onDidDismiss();
+        console.log('onDidDismiss resolved with role', role);
+
         return
     }
 
@@ -18,4 +31,10 @@ addButton.addEventListener('click', () => {
     newItem.innerHTML = `<strong>${enteredCourseName}:</strong>&nbsp;${enteredCourseRating}/5`;
 
     courseList.appendChild(newItem);
+    clearInputs();
 })
+
+const clearInputs = () => {
+    courseNameInput.value = '';
+    courseRatingInput.value = '';
+};
